51534b58a4f3bf148983012bb54cf4c5ce6144e6
Christian Neal <christianneal@Christians-MacBook-Air.local>
2015-09-02 14:34:43 -0500

    Initial movies

--- Tacos.rb
+++ Tacos.rb
@@ -1,17 +1,6 @@
 truth = "Tacos must not have lettuce only meat and cheese and salsa"
 
-truth = truth.split(" ")
 
-truth.each do |word|
-  if word.split(//).count == 5
-    puts word
-  end
-end
-# end
-# puts truth
-#
-# truth.each do |word|
-#   if word.count == 5
-#     puts word
-# end
-# end
+puts truth.split(" ")
+          .select { |word| word.split(//).count == 5}
+          .count
