c5e46b38ab8053a111ecbab9f55f4da20238b90b
Christian Neal <christianneal@Christians-MacBook-Air.local>
2015-09-02 15:50:15 -0500

    final version of Tacos

--- Tacos.rb
+++ Tacos.rb
@@ -1,5 +1,11 @@
 truth = "Tacos must not have lettuce only meat and cheese and salsa"
 
+puts "1. Here is the phrase in an array"
+print truth.split(" ")
+
+puts
+
+puts "2. The number of words in this array that have a length of 5 is:"
 puts truth.split(" ")
           .select { |word| word.length == 5}
           .count
