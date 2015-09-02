1a4485297141d946873bbe3dbe631ae4dd6bf79d
Christian Neal <christianneal@Christians-MacBook-Air.local>
2015-09-02 14:37:23 -0500

    fixed the length thing

--- Tacos.rb
+++ Tacos.rb
@@ -1,6 +1,5 @@
 truth = "Tacos must not have lettuce only meat and cheese and salsa"
 
-
 puts truth.split(" ")
-          .select { |word| word.split(//).count == 5}
+          .select { |word| word.length == 5}
           .count
